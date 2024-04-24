f3b1822cf3cc0f067d24d221ae948714a51a00dd
Jacinto Mba Cantero <jacmba@gmail.com>
2024-04-24 09:18:59 +0100

    Initial commit

---
+++ src/main/java/net/jazbelt/springboot3oauth/Springboot3OauthApplication.java
@@ -0,0 +1,13 @@
+package net.jazbelt.springboot3oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3OauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3OauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/jazbelt/springboot3oauth/Springboot3OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jazbelt.springboot3oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3OauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
